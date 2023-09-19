53309b7a277b6aad0d9c409388906d37ccbbf707
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-09-19 08:49:38 +0200

    change divs more semantig tags

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -15,8 +15,8 @@ export const Features = (props) => {
       <>
         <div className={'feature'}>
           <img className={'feature__image'} src={`${item.image}`} alt="responsive"></img>
-          <div className={'feature__description'}>{item.title}</div>
-          <div className={'feature__text'}>{item.text}</div>
+          <h4 className={'feature__description'}>{item.title}</h4>
+          <p className={'feature__text'}>{item.text}</p>
         </div>
       </>
     )
