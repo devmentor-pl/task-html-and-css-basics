a81df8b96066a17049123c70b168e552796d7723
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-09-23 17:43:58 +0200

    clean code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 
-import React, { Fragment } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
 import data from '../data';
 import './styles/reset/reset.css'
-import './styles/sass/main.css'
+import './styles/sass/style.css'
 import { Header } from './components/Header';
-import { Navigation } from './components/Navigation/Navigation';
 import { Main } from './components/Main';
 import Footer from './components/Footer'
-
-import { toggleHamburger } from './js/toggleHamburger';
-import { selectMember } from './js/selectMember';
 
 
 const root = createRoot(document.querySelector('#root'));
@@ -27,11 +22,6 @@ const App = () => (
 root.render(<App></App>);
 
 
-// const iconsHamburger = document.querySelector('.hamburger__icon');
-// toggleHamburger(iconsHamburger)
-
-// const selectElement = document.getElementById("select__member");
-// selectMember(selectElement)
 
 
 
--- src/js/toggleHamburger.js
+++
@@ -1,7 +0,0 @@
-export const toggleHamburger = (el) => {
-  el.addEventListener('click', (event) => {
-    el.classList.toggle("open");
-  });
-}
-
-export default toggleHamburger
